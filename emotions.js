5c977456dd06a42152136c97b4ea8d58f44f1293
Stryo <74878256+Stryo@users.noreply.github.com>
2021-02-26 17:39:34 +0200

    Add files via upload

---
+++ emotions.js
@@ -0,0 +1,132 @@
+var data = { fear:[{cpercentage: 32.3, color: "#493628", offset: 0, colorHSL: 0},
+                    {cpercentage: 16.6, color: "#13110f", offset: 0, colorHSL: 0},
+                    {cpercentage: 12.0, color: "#7d7976", offset: 0, colorHSL: 0},
+                    {cpercentage: 10.8, color: "#e3dacc", offset: 0, colorHSL: 0},
+                    {cpercentage: 9.5, color: "#bfad80", offset:0, colorHSL: 0},
+                    {cpercentage: 6.5 , color: "#464f2b", offset:0, colorHSL: 0},
+                    {cpercentage: 4.9 , color: "#96836d", offset:0, colorHSL: 0},
+                    {cpercentage: 3.7 , color: "#cc7e4", offset:0, colorHSL: 0},
+                    {cpercentage: 3.0 , color: "#99613f", offset:0, colorHSL: 0},
+                    {cpercentage: 0.8 , color: "#d9e382", offset:0, colorHSL: 0}],
+
+            shyness:[{cpercentage: 29.2, color: "#796344", offset: 0, colorHSL: 0},
+                    {cpercentage: 26.3, color: "#b4b3ad", offset: 0, colorHSL: 0},
+                    {cpercentage: 19.3, color: "#292714", offset: 0, colorHSL: 0},
+                    {cpercentage: 9.9, color: "#c2b093", offset: 0, colorHSL: 0},
+                    {cpercentage: 4.3, color: "#2c5046", offset:0, colorHSL: 0},
+                    {cpercentage: 3.6 , color: "#867e38", offset:0, colorHSL: 0},
+                    {cpercentage: 2.9 , color: "#4a2f25", offset:0, colorHSL: 0},
+                    {cpercentage: 2.6 , color: "#729566", offset:0, colorHSL: 0},
+                    {cpercentage: 1.3 , color: "#d6c17c", offset:0, colorHSL: 0},
+                    {cpercentage: 0.8 , color: "#a8bbcd", offset:0, colorHSL: 0}],
+
+            surprise:[{cpercentage: 43.0, color: "#b69666", offset: 0, colorHSL: 0},
+                    {cpercentage: 34.0, color: "#736a63", offset: 0, colorHSL: 0},
+                    {cpercentage: 13.4, color: "#e2e3db", offset: 0, colorHSL: 0},
+                    {cpercentage: 4.6, color: "#d87d38", offset: 0, colorHSL: 0},
+                    {cpercentage: 2.3, color: "#237964", offset:0, colorHSL: 0},
+                    {cpercentage: 0.9 , color: "#a92b2a", offset:0, colorHSL: 0},
+                    {cpercentage: 0.7 , color: "#8e4438", offset:0, colorHSL: 0},
+                    {cpercentage: 0.5 , color: "#f2d65a", offset:0, colorHSL: 0},
+                    {cpercentage: 0.4 , color: "#50674d", offset:0, colorHSL: 0},
+                    {cpercentage: 0.2 , color: "#322310", offset:0, colorHSL: 0}],
+
+            trust : [ {cpercentage: 53.0, color: "#433e39", offset: 0, colorHSL: 0},
+                    {cpercentage: 25.9, color: "#b89c68", offset: 0, colorHSL: 0},
+                    {cpercentage: 4.3, color: "#a5993f", offset: 0, colorHSL: 0},
+                    {cpercentage: 3.9, color: "#eeb75d", offset: 0, colorHSL: 0},
+                    {cpercentage: 3.9, color: "#5f482d", offset:0, colorHSL: 0},
+                    {cpercentage: 3.1 , color: "#6c3b2d", offset:0, colorHSL: 0},
+                    {cpercentage: 2.4 , color: "#efe9cf", offset:0, colorHSL: 0},
+                    {cpercentage: 1.5 , color: "#c86c48", offset:0, colorHSL: 0},
+                    {cpercentage: 1.0 , color: "#57501f", offset:0, colorHSL: 0},
+                    {cpercentage: 1.0 , color: "#9a6240", offset:0, colorHSL: 0}],
+
+            sadness : [ {cpercentage: 50.7, color: "#2d2d2d", offset: 0, colorHSL: 0},
+                    {cpercentage: 11.6, color: "#aca393", offset: 0, colorHSL: 0},
+                    {cpercentage: 8.2, color: "#4c5534", offset: 0, colorHSL: 0},
+                    {cpercentage: 8.1, color: "#1d1b44", offset: 0, colorHSL: 0},
+                    {cpercentage: 8.0, color: "#b99166", offset:0, colorHSL: 0},
+                    {cpercentage: 7.9 , color: "#705339", offset:0, colorHSL: 0},
+                    {cpercentage: 2.8 , color: "#214451", offset:0, colorHSL: 0},
+                    {cpercentage: 2.3 , color: "#e48e4b", offset:0, colorHSL: 0},
+                    {cpercentage: 0.3 , color: "#f0f589", offset:0, colorHSL: 0},
+                    {cpercentage: 0.1 , color: "#c2a254", offset:0, colorHSL: 0}],
+
+            disgust : [ {cpercentage: 24.7, color: "#68231b", offset: 0, colorHSL: 0},
+                    {cpercentage: 23.1, color: "#b48246", offset: 0, colorHSL: 0},
+                    {cpercentage: 18.9, color: "#ceaf92", offset: 0, colorHSL: 0},
+                    {cpercentage: 15.5, color: "#535042", offset: 0, colorHSL: 0},
+                    {cpercentage: 6.8, color: "#364879", offset:0, colorHSL: 0},
+                    {cpercentage: 6.0 , color: "#d5735e", offset:0, colorHSL: 0},
+                    {cpercentage: 2.3 , color: "#de8837", offset:0, colorHSL: 0},
+                    {cpercentage: 1.8 , color: "#5b875d", offset:0, colorHSL: 0},
+                    {cpercentage: 0.7 , color: "#868092", offset:0, colorHSL: 0},
+                    {cpercentage: 0.1 , color: "#cce1d6", offset:0, colorHSL: 0}],
+
+            anger : [ {cpercentage: 36.7, color: "#b09b97", offset: 0, colorHSL: 0},
+                    {cpercentage: 18.6, color: "#322628", offset: 0, colorHSL: 0},
+                    {cpercentage: 12.3, color: "#a8635d", offset: 0, colorHSL: 0},
+                    {cpercentage: 9.6, color: "#d23c33", offset: 0, colorHSL: 0},
+                    {cpercentage: 8.6, color: "#0a2726", offset:0, colorHSL: 0},
+                    {cpercentage: 6.0 , color: "#a94d33", offset:0, colorHSL: 0},
+                    {cpercentage: 4.5 , color: "#848c4c", offset:0, colorHSL: 0},
+                    {cpercentage: 2.6 , color: "#2b6792", offset:0, colorHSL: 0},
+                    {cpercentage: 0.8 , color: "#494d37", offset:0, colorHSL: 0},
+                    {cpercentage: 0.2 , color: "#e99252", offset:0, colorHSL: 0}],
+
+            anticipation : [ {cpercentage: 31.8, color: "#413f4b", offset: 0, colorHSL: 0},
+                    {cpercentage: 24.6, color: "#dbd3cf", offset: 0, colorHSL: 0},
+                    {cpercentage: 13.7, color: "#ddc38e", offset: 0, colorHSL: 0},
+                    {cpercentage: 9.9, color: "#616b52", offset: 0, colorHSL: 0},
+                    {cpercentage: 5.8, color: "#cb989f", offset:0, colorHSL: 0},
+                    {cpercentage: 4.9 , color: "#95b267", offset:0, colorHSL: 0},
+                    {cpercentage: 4.4 , color: "#b25147", offset:0, colorHSL: 0},
+                    {cpercentage: 2.8 , color: "#b9451f", offset:0, colorHSL: 0},
+                    {cpercentage: 1.3 , color: "#218940", offset:0, colorHSL: 0},
+                    {cpercentage: 0.7 , color: "#ded170", offset:0, colorHSL: 0}]
+
+                    
+
+                    
+                
+                
+                };
+
+var dataGradient = [{colorFear:0, offsetFearPer:0, offsetFearNum:0, colorShyness: 0, offsetShynessPer:0, offsetShynessNum:0, colorSurprise:0, offsetSurpriseNum:0, offsetSurprisePer:0, colorTrust:0, offsetTrustPer:0, offsetTrustNum:0, colorSadness:0, offsetSadnessPer:0, offsetSadnessNum:0, colorDisgust:0, offsetDisgustPer:0, offsetDisgustNum:0, colorAnger:0, offsetAngerPer:0, offsetAngerNum:0, colorAnticipation:0, offsetAnticipationPer:0, offsetAnticipationNum:0 },
+                {colorFear:0, offsetFearPer:0, offsetFearNum:0, colorShyness: 0, offsetShynessPer:0, offsetShynessNum:0, colorSurprise:0, offsetSurpriseNum:0, offsetSurprisePer:0, colorTrust:0, offsetTrustPer:0, offsetTrustNum:0, colorSadness:0, offsetSadnessPer:0, offsetSadnessNum:0, colorDisgust:0, offsetDisgustPer:0, offsetDisgustNum:0, colorAnger:0, offsetAngerPer:0, offsetAngerNum:0, colorAnticipation:0, offsetAnticipationPer:0, offsetAnticipationNum:0 },
+                {colorFear:0, offsetFearPer:0, offsetFearNum:0, colorShyness: 0, offsetShynessPer:0, offsetShynessNum:0, colorSurprise:0, offsetSurpriseNum:0, offsetSurprisePer:0, colorTrust:0, offsetTrustPer:0, offsetTrustNum:0, colorSadness:0, offsetSadnessPer:0, offsetSadnessNum:0, colorDisgust:0, offsetDisgustPer:0, offsetDisgustNum:0, colorAnger:0, offsetAngerPer:0, offsetAngerNum:0, colorAnticipation:0, offsetAnticipationPer:0, offsetAnticipationNum:0 },
+                {colorFear:0, offsetFearPer:0, offsetFearNum:0, colorShyness: 0, offsetShynessPer:0, offsetShynessNum:0, colorSurprise:0, offsetSurpriseNum:0, offsetSurprisePer:0, colorTrust:0, offsetTrustPer:0, offsetTrustNum:0, colorSadness:0, offsetSadnessPer:0, offsetSadnessNum:0, colorDisgust:0, offsetDisgustPer:0, offsetDisgustNum:0, colorAnger:0, offsetAngerPer:0, offsetAngerNum:0, colorAnticipation:0, offsetAnticipationPer:0, offsetAnticipationNum:0 },
+                {colorFear:0, offsetFearPer:0, offsetFearNum:0, colorShyness: 0, offsetShynessPer:0, offsetShynessNum:0, colorSurprise:0, offsetSurpriseNum:0, offsetSurprisePer:0, colorTrust:0, offsetTrustPer:0, offsetTrustNum:0, colorSadness:0, offsetSadnessPer:0, offsetSadnessNum:0, colorDisgust:0, offsetDisgustPer:0, offsetDisgustNum:0, colorAnger:0, offsetAngerPer:0, offsetAngerNum:0, colorAnticipation:0, offsetAnticipationPer:0, offsetAnticipationNum:0 },
+                {colorFear:0, offsetFearPer:0, offsetFearNum:0, colorShyness: 0, offsetShynessPer:0, offsetShynessNum:0, colorSurprise:0, offsetSurpriseNum:0, offsetSurprisePer:0, colorTrust:0, offsetTrustPer:0, offsetTrustNum:0, colorSadness:0, offsetSadnessPer:0, offsetSadnessNum:0, colorDisgust:0, offsetDisgustPer:0, offsetDisgustNum:0, colorAnger:0, offsetAngerPer:0, offsetAngerNum:0, colorAnticipation:0, offsetAnticipationPer:0, offsetAnticipationNum:0 },
+                {colorFear:0, offsetFearPer:0, offsetFearNum:0, colorShyness: 0, offsetShynessPer:0, offsetShynessNum:0, colorSurprise:0, offsetSurpriseNum:0, offsetSurprisePer:0, colorTrust:0, offsetTrustPer:0, offsetTrustNum:0, colorSadness:0, offsetSadnessPer:0, offsetSadnessNum:0, colorDisgust:0, offsetDisgustPer:0, offsetDisgustNum:0, colorAnger:0, offsetAngerPer:0, offsetAngerNum:0, colorAnticipation:0, offsetAnticipationPer:0, offsetAnticipationNum:0 },
+                {colorFear:0, offsetFearPer:0, offsetFearNum:0, colorShyness: 0, offsetShynessPer:0, offsetShynessNum:0, colorSurprise:0, offsetSurpriseNum:0, offsetSurprisePer:0, colorTrust:0, offsetTrustPer:0, offsetTrustNum:0, colorSadness:0, offsetSadnessPer:0, offsetSadnessNum:0, colorDisgust:0, offsetDisgustPer:0, offsetDisgustNum:0, colorAnger:0, offsetAngerPer:0, offsetAngerNum:0, colorAnticipation:0, offsetAnticipationPer:0, offsetAnticipationNum:0 },
+                {colorFear:0, offsetFearPer:0, offsetFearNum:0, colorShyness: 0, offsetShynessPer:0, offsetShynessNum:0, colorSurprise:0, offsetSurpriseNum:0, offsetSurprisePer:0, colorTrust:0, offsetTrustPer:0, offsetTrustNum:0, colorSadness:0, offsetSadnessPer:0, offsetSadnessNum:0, colorDisgust:0, offsetDisgustPer:0, offsetDisgustNum:0, colorAnger:0, offsetAngerPer:0, offsetAngerNum:0, colorAnticipation:0, offsetAnticipationPer:0, offsetAnticipationNum:0 },
+                {colorFear:0, offsetFearPer:0, offsetFearNum:0, colorShyness: 0, offsetShynessPer:0, offsetShynessNum:0, colorSurprise:0, offsetSurpriseNum:0, offsetSurprisePer:0, colorTrust:0, offsetTrustPer:0, offsetTrustNum:0, colorSadness:0, offsetSadnessPer:0, offsetSadnessNum:0, colorDisgust:0, offsetDisgustPer:0, offsetDisgustNum:0, colorAnger:0, offsetAngerPer:0, offsetAngerNum:0, colorAnticipation:0, offsetAnticipationPer:0, offsetAnticipationNum:0 }]
+
+
+var dataButtons = [{
+                        emotion: "fear",
+                    },
+                    {
+                        emotion: "joy"
+                    },
+                    {
+                        emotion:"surprise"
+                    },
+                    {
+                        emotion:"trust"
+                    },
+                    {
+                        emotion:"sadness"
+                    },
+                    {
+                        emotion:"disgust"
+                    },
+                    {
+                        emotion:"anger"
+                    },
+                    {
+                        emotion:"anticipation"
+                    }
+                    ];
